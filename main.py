8c6d47d3f2944e910ecabffa9ebcefbdb9af31f9
Dermot Fitzpatrick <39203833+tomredf@users.noreply.github.com>
2021-10-05 23:46:06 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# TODO: Do any required imports
+
+from data import MENU
+from data import resources
+
+
+# TODO: Prompt user for what they would like (espresso/latte/cappuccino):
+
+
+# TODO: Turn off the coffee machine
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi(f"Resources: {resources} \nMenu: {MENU}")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
