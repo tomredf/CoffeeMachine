65e42c2e1390aaf8524742e47a881303d56d2891
Dermot Fitzpatrick <39203833+tomredf@users.noreply.github.com>
2021-10-07 00:39:24 -0700

    Building out functions. Added logo

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-# TODO: Do any required imports first
-
 
 from data import MENU
 from data import resources
+from art import logo
 
 
 def make_espresso():
-    return 0
+    cost = MENU["espresso"]["cost"]
+    water = MENU["espresso"]["ingredients"]["water"]
+    coffee = MENU["espresso"]["ingredients"]["coffee"]
+    enough_water = check_resources("water", water)
+    if not enough_water:
+        print("Sorry there is not enough water.")
+        return 0
+    enough_coffee = check_resources("coffee", coffee)
+    if not enough_coffee:
+        print("Sorry there is not enough coffee.")
+        return 0
+    print(f"Please pay: ${cost}")
+    return cost
 
 
 def make_latte():
@@ -17,32 +28,43 @@ def make_cappuccino():
     return 0
 
 
-def run_report():
-    return 0
+def run_report(money):
+    print("Available Resources:")
+    print(f"Water: {resources['water']}ml")
+    print(f"Milk: {resources['milk']}ml")
+    print(f"Coffee: {resources['coffee']}g")
+    print(f"Money: ${money}")
+
+
+def check_resources(resource, required):
+    available = resources[resource]
+    if available < required:
+        return False
+    else:
+        return True
 
 
 def make_coffee():
-    # TODO: Prompt user for what they would like (espresso/latte/cappuccino):
-    coffee = input("What would you like? (espresso/latte/cappuccino): ")
+    print(logo)
+    money = 0
+    is_on = True
+    while is_on:
+        request = input("What would you like? (espresso/latte/cappuccino): ")
 
-    if coffee == "espresso":
-        make_espresso()
-    elif coffee == "latte":
-        make_latte()
-    elif coffee == "cappuccino:":
-        make_cappuccino()
-    elif coffee == "off":
-        return
-    elif coffee == "report:":
-        run_report()
-    else:
-        return
-
-    # TODO: Turn off the coffee machine with secret word
-
-    # TODO: Add print report function
-
-    # TODO: Add check sufficient resources function
+        if request == "espresso":
+            make_espresso()
+        elif request == "latte":
+            make_latte()
+        elif request == "cappuccino":
+            make_cappuccino()
+        elif request == "off":
+            print("Coffee Machine is turned off.")
+            is_on = False
+            return
+        elif request == "report":
+            run_report(money)
+        else:
+            return
 
 
 def print_hi(name):
@@ -52,6 +74,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi(f"Resources: {resources} \nMenu: {MENU}")
+    make_coffee()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
