eb30b7f785e440dc958b77dfdc8aa49e29816ff6
Dermot Fitzpatrick <39203833+tomredf@users.noreply.github.com>
2021-10-06 19:07:02 -0700

    setting up framework for code

--- main.py
+++ main.py
@@ -5,16 +5,44 @@ from data import MENU
 from data import resources
 
 
-# TODO: Prompt user for what they would like (espresso/latte/cappuccino):
+def make_espresso():
+    return 0
 
 
-# TODO: Turn off the coffee machine with secret word
+def make_latte():
+    return 0
 
 
-# TODO: Add print report function
+def make_cappuccino():
+    return 0
 
 
-# TODO: Add check sufficient resourese function
+def run_report():
+    return 0
+
+
+def make_coffee():
+    # TODO: Prompt user for what they would like (espresso/latte/cappuccino):
+    coffee = input("What would you like? (espresso/latte/cappuccino): ")
+
+    if coffee == "espresso":
+        make_espresso()
+    elif coffee == "latte":
+        make_latte()
+    elif coffee == "cappuccino:":
+        make_cappuccino()
+    elif coffee == "off":
+        return
+    elif coffee == "report:":
+        run_report()
+    else:
+        return
+
+    # TODO: Turn off the coffee machine with secret word
+
+    # TODO: Add print report function
+
+    # TODO: Add check sufficient resources function
 
 
 def print_hi(name):
@@ -25,6 +53,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi(f"Resources: {resources} \nMenu: {MENU}")
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
